739c1c4771ffc4cd9f6e4db34b3464a1270aefd9
Kenichi Kamiya <kachick1@gmail.com>
2024-11-10 17:53:47 +0900

    Remove outdated dependencies in flake to follow GH-21

--- flake.nix
+++ flake.nix
@@ -30,8 +30,7 @@
                 dprint
                 typos
 
-                nixpkgs-review # TODO: Replace with nixos-unstable as same as workflow
-                gnutar
+                # nixpkgs-review # TODO: Enable since updated to https://github.com/Mic92/nixpkgs-review/commit/055465e55d131ffb1e1617f46d3bade0b87bbe69 or higher
                 gh
                 git
               ];
