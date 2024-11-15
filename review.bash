9d17d0397ac36ef98c34c65b1dbc68d356b52bf1
Kenichi Kamiya <kachick1@gmail.com>
2024-11-15 23:04:59 +0900

    Roughly extend waiting time

--- review.bash
+++ review.bash
@@ -6,7 +6,7 @@ pr_number="$1"
 output_dir="$(mktemp --directory)"
 
 gh workflow run 'nixpkgs-review.yml' --field pr-number="$pr_number"
-sleep 3
+sleep 10 # TODO: Ensure to get correct ID
 run_id="$(gh run list --workflow=nixpkgs-review.yml --branch main --limit 1 --json databaseId --jq '.[].databaseId')"
 
 gh run watch "$run_id" # Don't use --exit-status to make sure the downloading
