c1e943ca1523b87e7c26b9ad570af27305e8cd7b
Kenichi Kamiya <kachick1@gmail.com>
2025-02-08 02:18:50 +0900

    Adjust output for GitHub Action summary

--- resume.bash
+++ resume.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
+
+if [[ "$CI" != 'true' ]]; then
+  set -x
+fi
 
 run_id="$1"
 output_dir="$(mktemp --tmpdir --directory "nixpkgs-reviewing-workspace.run-${run_id}.XXXX")"
@@ -10,12 +14,15 @@ if [[ "$CI" != 'true' ]]; then
   # Replace with like a `gh workflow run --watch` if https://github.com/cli/cli/issues/3559 is resolved
   gh run watch "$run_id" --interval 10 # Don't use --exit-status to make sure the downloading
 fi
-gh run download "$run_id" --dir "$output_dir"
 
-echo "Downloaded the files in $output_dir"
+{
+  gh run download "$run_id" --dir "$output_dir"
 
-tree "$output_dir"
+  echo "Downloaded the files in $output_dir"
 
-fd --absolute-path . "$output_dir"
+  tree "$output_dir"
+
+  fd --absolute-path . "$output_dir"
+} >&2
 
 fd --absolute-path report.md "$output_dir" | ruby ./sort.rb | ruby ./concat.rb
