fc2b74f76f1e77481aeb0a19ce1480dba033ec8f
Kenichi Kamiya <kachick1@gmail.com>
2025-05-14 22:45:51 +0900

    Update nix fmt convention

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     rec {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
       devShells = forAllSystems (
         system:
         let
